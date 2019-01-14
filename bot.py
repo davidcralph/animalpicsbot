af3bb41a8d52adc889bf48d941112623eaad9b7c
David Ralph <ohlookitsderpy@protonmail.com>
2019-01-14 18:02:51 +0000

    bit of rewrite xd

--- bot.py
+++ bot.py
@@ -1,26 +1,24 @@
 import discord
+from discord.ext import commands
 import asyncio
 import aiohttp
 
-class BotClient(discord.Client):
-    async def on_ready(self):
-        print('[Discord] Connected!')
+client = commands.Bot(command_prefix='a?')
 
-    async def on_message(self, msg):
-        if msg.author == self.user:
-            return
-            
-        if msg.content.startswith('a?cat'):
-            async with aiohttp.ClientSession() as req:
-             async with req.get('https://aws.random.cat/meow') as cat:
-              cat = await cat.json()
-              return await msg.channel.send(cat["file"])
+@client.command()
+async def cat(msg):
+ if msg.author.bot: return
+ async with aiohttp.ClientSession() as req:
+    async with req.get('https://aws.random.cat/meow') as cat:
+        cat = await cat.json()
+        return await msg.channel.send(cat["file"])
 
-        if msg.content.startswith('a?dog'):
-           async with aiohttp.ClientSession() as req:
-            async with req.get('https://dog.ceo/api/breeds/image/random') as dog:
-             dog = await dog.json()
-             return await msg.channel.send(dog["message"])
+@client.command()
+async def dog(msg):
+ if msg.author.bot: return
+ async with aiohttp.ClientSession() as req:
+    async with req.get('https://dog.ceo/api/breeds/image/random') as dog:
+        dog = await dog.json()
+        return await msg.channel.send(dog["message"])
 
-client = BotClient()
 client.run('haha no')
