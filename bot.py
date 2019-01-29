b321e6d8632e3c9d8d6a352545fe3c779df33002
David Ralph <ohlookitsderpy@protonmail.com>
2019-01-29 18:10:02 +0000

    Add more commands

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
+# imports
 import discord
 from discord.ext import commands
 import asyncio
 import aiohttp
 
-client = commands.Bot(command_prefix='a?')
+client = commands.Bot(command_prefix='a?') # prefix
 
+# random.cat
 @client.command()
 async def cat(msg):
  if msg.author.bot: return
@@ -13,6 +15,7 @@ async def cat(msg):
         cat = await cat.json()
         return await msg.channel.send(cat['file'])
 
+# dog.ceo
 @client.command()
 async def dog(msg):
  if msg.author.bot: return
@@ -20,13 +23,56 @@ async def dog(msg):
     async with req.get('https://dog.ceo/api/breeds/image/random') as dog:
         dog = await dog.json()
         return await msg.channel.send(dog['message'])
-     
+
+# animals.anidiots.guide     
 @client.command()
 async def penguin(msg):
  if msg.author.bot: return
  async with aiohttp.ClientSession() as req:
-    async with req.get('https://animals.anidiots.guide/penguin') as penguin:
+    async with req.get('http://animals.anidiots.guide/penguin') as penguin:
         penguin = await penguin.json()
-        return await msg.channel.send(penguin['link'])     
-     
+        return await msg.channel.send(penguin['link'])
+
+@client.command()
+async def panda(msg):
+ if msg.author.bot: return
+ async with aiohttp.ClientSession() as req:
+    async with req.get('http://animals.anidiots.guide/panda') as panda:
+        panda = await panda.json()
+        return await msg.channel.send(panda['link'])   
+
+@client.command()
+async def tiger(msg):
+ if msg.author.bot: return
+ async with aiohttp.ClientSession() as req:
+    async with req.get('http://animals.anidiots.guide/tiger') as tiger:
+        tiger = await tiger.json()
+        return await msg.channel.send(tiger['link'])
+
+@client.command()
+async def lion(msg):
+ if msg.author.bot: return
+ async with aiohttp.ClientSession() as req:
+    async with req.get('http://animals.anidiots.guide/lion') as lion:
+        lion = await lion.json()
+        return await msg.channel.send(lion['link'])
+
+@client.command()
+async def redpanda(msg):
+ if msg.author.bot: return
+ async with aiohttp.ClientSession() as req:
+    async with req.get('http://animals.anidiots.guide/red_panda') as redpanda:
+        redpanda = await redpanda.json()
+        return await msg.channel.send(redpanda['link'])    
+
+# nekos.life
+@client.command()
+async def lizard(msg):
+ if msg.author.bot: return
+ async with aiohttp.ClientSession() as req:
+    async with req.get('https://nekos.life/api/lizard') as lizard:
+        lizard = await lizard.json()
+        return await msg.channel.send(lizard['url'])       
+
+# login
 client.run('haha no')
